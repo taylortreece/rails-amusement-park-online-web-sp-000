ecaa7271f7a47f94031c828b930bfb8450f2c4c9
taylortreece <tayloratreece@gmail.com>
2021-05-13 15:38:33 -0500

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+    
+    def show
+        @user = User.find_by(id: params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login(@user)
+            redirect_to @user
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
+
+    def login(user)
+        session[:user_id] = user.id
+    end
+end
